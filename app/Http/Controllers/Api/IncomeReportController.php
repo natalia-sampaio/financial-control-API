2d0c9ed2cb7bdef1b071d9eb64e8750cdc2b1dbf
Natália Sampaio Rosa e Silva <natalia.srs@outlook.com>
2022-08-04 15:51:06 -0300

    CRUD for income reports ready

--- app/Http/Controllers/Api/IncomeReportController.php
+++ app/Http/Controllers/Api/IncomeReportController.php
@@ -23,28 +23,48 @@ class IncomeReportController
     public function store(IncomeReportRequest $request)
     {
         $data = $request;
-        
-        $description = $data->description;
-        
-        $date = $data->date_of_income;
-        $monthOfIncome = Carbon::createFromFormat('Y-m-d', $date)->format('m');
-        
-        $verificationQuery = DB::table('income_reports')
-            ->where('description', $description)
-            ->whereMonth('date_of_income', $monthOfIncome)
-            ->exists();
-
-        if ($verificationQuery) {
+        if ($this->verifyDuplicates($data)) {
             return response()->json([
                 'message' => "An income named '{$data->description}' has already been reported this month."
             ], 444);
-        };
-        
+        }
         return response()->json($this->incomeReportRepository->add($data), 201);   
     }
 
     public function show(IncomeReport $income)
     {
         return $income;
+    }
+
+    public function update(int $income, IncomeReportRequest $request)
+    {
+        $data = $request;
+        if ($this->verifyDuplicates($data, $income)) {
+            return response()->json([
+                'message' => "An income named '{$data->description}' has already been reported this month."
+            ], 444);
+        }
+        $income = IncomeReport::where('id', $income)->update($data->all());
+        return response()->json(["success" => "Income updated.", "income" => $income], 200);
+    }
+
+    public function destroy(int $income)
+    {
+        IncomeReport::destroy($income);
+        return response()->noContent();
+    }
+
+    private function verifyDuplicates(IncomeReportRequest $request, int $income = null)
+    {
+        $data = $request;
+        $description = $data->description;
+        $dateOfIncome = $data->date_of_income;
+        $monthOfIncome = Carbon::createFromFormat('Y-m-d', $dateOfIncome)->format('m');
+
+        return DB::table('income_reports')
+            ->whereNot('id', $income)
+            ->where('description', $description)
+            ->whereMonth('date_of_income', $monthOfIncome)
+            ->exists();
     }
 }

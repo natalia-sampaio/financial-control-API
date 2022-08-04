27d2344df1b23c5d715d8fed551dbb5fcd05fa9a
Natália Sampaio Rosa e Silva <natalia.srs@outlook.com>
2022-08-04 00:32:42 -0300

    wip CRUD income - Create and Read income reports

--- app/Http/Controllers/Api/IncomeReportController.php
+++ app/Http/Controllers/Api/IncomeReportController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\IncomeReportRequest;
-use App\Http\Requests\IncomeRequest;
+use App\Models\IncomeReport;
 use App\Repositories\IncomeReportRepository;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class IncomeReportController
 {
@@ -12,10 +14,37 @@ class IncomeReportController
     {
         
     }
+
+    public function index()
+    {
+        return IncomeReport::all();
+    }
     
     public function store(IncomeReportRequest $request)
     {
         $data = $request;
-        return response()->json($this->incomeReportRepository->add($data), 201);
+        
+        $description = $data->description;
+        
+        $date = $data->date_of_income;
+        $monthOfIncome = Carbon::createFromFormat('Y-m-d', $date)->format('m');
+        
+        $verificationQuery = DB::table('income_reports')
+            ->where('description', $description)
+            ->whereMonth('date_of_income', $monthOfIncome)
+            ->exists();
+
+        if ($verificationQuery) {
+            return response()->json([
+                'message' => "An income named '{$data->description}' has already been reported this month."
+            ], 444);
+        };
+        
+        return response()->json($this->incomeReportRepository->add($data), 201);   
+    }
+
+    public function show(IncomeReport $income)
+    {
+        return $income;
     }
 }
